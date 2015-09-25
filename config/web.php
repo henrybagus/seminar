49210474e779885ede0839bd8f446317510f0883
henrybagus <7312004@student.unpar.ac.id>
2015-09-25 12:09:12 +0700

    notif2

--- config/web.php
+++ config/web.php
@@ -23,13 +23,17 @@ $config = [
         'errorHandler' => [
             'errorAction' => 'backend/site/error',
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'mailer' => [
+            'class' => 'nickcv\mandrill\Mailer',
+            'apikey' => 'iUAwIgOl1Fr6uI1nWFa5dA',
         ],
+        // 'mailer' => [
+        //     'class' => 'yii\swiftmailer\Mailer',
+        //     // send all mails to a file by default. You have to set
+        //     // 'useFileTransport' to false and configure a transport
+        //     // for the mailer to send real emails.
+        //     'useFileTransport' => true,
+        // ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
