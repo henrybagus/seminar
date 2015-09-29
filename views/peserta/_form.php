45c0748a83aed38ce6098f98d6dc716077032e32
Theo <Theodoruskurniawan64@gmail.com>
2015-09-29 17:31:28 +0700

    edit create and _form view seminar
    edit view

--- views/peserta/_form.php
+++ views/peserta/_form.php
@@ -7,35 +7,43 @@ use yii\helpers\ArrayHelper;
 /* @var $this yii\web\View */
 /* @var $model app\models\Peserta */
 /* @var $form yii\widgets\ActiveForm */
+
+$this->registerCss(".listsemninar col-lg-6{
+	border: 1px solid red;
+}");
+$this->registerCss(".checkbox{
+	background: #;
+	height: 16px;
+	width: 16px;
+	display:inline-block;
+	padding: 0 0 0 0px;
+");
 ?>
+<?php $form = ActiveForm::begin(); ?>
+	<div class="row">
+		<div class="form col-lg-4">
+			<?php echo "Booked for logo"?>
+		</div>
+		<div class="form col-lg-4">
+			<?= $form->field($model, 'nama')->textInput(['maxlength' => 255]) ?>
 
-<div class="peserta-form">
+			<?= $form->field($model, 'hp')->textInput(['maxlength' => 255]) ?>
 
-    <?php $form = ActiveForm::begin(); ?>
+			<?= $form->field($model, 'email')->input('email') ?>
 
-    <?= $form->field($model, 'nama')->textInput(['maxlength' => 255]) ?>
+			<?= $form->field($model, 'universitas')->textInput(['maxlength' => 255]) ?>
 
-    <?= $form->field($model, 'hp')->textInput(['maxlength' => 255]) ?>
+			<?= $form->field($model, 'jurusan')->textInput(['maxlength' => 255]) ?>
 
-    <?= $form->field($model, 'email')->input('email') ?>
-
-    <?= $form->field($model, 'universitas')->textInput(['maxlength' => 255]) ?>
-
-    <?= $form->field($model, 'jurusan')->textInput(['maxlength' => 255]) ?>
-
-    <?= $form->field($model, 'npm')->textInput(['maxlength' => 255]) ?>
-	
-	<div class="seminarlist">
-		</br>
-		<?php
-			echo $form->field($model, 'events')->checkboxList(ArrayHelper::map($model_event,"id","nama"),['separator'=>'</br></br>'])->label('<h3>Seminar yang anda minati:</h3></br></br>');
-		?>
+			<?= $form->field($model, 'npm')->textInput(['maxlength' => 255]) ?>
+		</div>
+		<div class="listsemninar col-lg-4">
+			<?php
+				echo $form->field($model, 'events')->checkboxList(ArrayHelper::map($model_event,"id","nama"),['separator'=>'</br></br>'])->label('<h3>Seminar yang anda minati:</h3>');
+			?>
+		</div>
+		<div class="form-group">
+			<?= Html::submitButton($model->isNewRecord ? 'Daftar' : 'Update', ['class' => $model->isNewRecord ? 'btn 	btn-success btn-block' : 'btn btn-primary btn-block']) ?>
+		</div>
 	</div>
-	<br><br>
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
-</div>
+<?php ActiveForm::end(); ?>
--- views/peserta/create.php
+++ views/peserta/create.php
@@ -5,16 +5,17 @@ use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Peserta */
-
-
 $this->title = 'Daftar Peserta Seminar ';
-
 $this->params['breadcrumbs'][] = $this->title;
+
+$this->registerCss(".title{
+	text-align: center;
+}");
 ?>
 <div class="peserta-create">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
+	<div class="title">
+		<h1><?= Html::encode($this->title) ?></h1>
+	</div>
     <?= $this->render('_form', [
         'model' => $model,
 		'model_event'=>$model_event,
