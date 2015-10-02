2457e43e5f027bff3993c1c315ea71421438955f
wilianto <indrawan.wilianto@gmail.com>
2015-10-02 15:10:01 +0700

    hanya ubah sedikit saja

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=cdc_uces_seminar',
+    'dsn' => 'mysql:host=127.0.0.1:3306;dbname=cdc_uces_seminar',
     'username' => 'root',
-    'password' => '',
+    'password' => 'admin123',
     'charset' => 'utf8',
 ];
